b7385c1f8aa1db9d0dd167b0846abd664ac2c18b
Sophia Snipes <snipeso@users.noreply.github.com>
2023-08-14 20:48:39 +0200

    dynamic legend

--- ExamplePlot.m
+++ ExamplePlot.m
@@ -18,26 +18,31 @@ SleepEnds = SleepStarts + 8;
 SleepMidpoint = 3.5; % circadian midpoint of sleep
 
 figure('units','centimeters','position',[0 0 25, 13])
-
+ylim([0 4])
 hold on
 
+Legend = {};
 
 % sleep deprivation homeostatic curve
 Format.Color = getColors(1, '', 'red');
 plot2process(SleepStarts([1 3]), SleepEnds([1 3]), SleepMidpoint,  'homeostatic', Format);
 
+Legend = cat(2, Legend, 'Sleep deprivation');
+
 % homeostatic curve
 Format.Color = getColors(1, '', 'yellow');
 plot2process(SleepStarts, SleepEnds, SleepMidpoint, 'homeostatic',  Format);
 
+Legend = cat(2, Legend, 'Homeostatic process');
+
 % circadian cycle
 Format.Color = getColors(1, '', 'blue');
 plot2process(SleepStarts, SleepEnds, SleepMidpoint, 'circadian',  Format);
-
+Legend = cat(2, Legend, 'Circadian process');
 
 % background information
 Format.Color = 'k';
 plot2process(SleepStarts, SleepEnds, SleepMidpoint, 'labels', Format);
 
-legend({'Sleep deprivation', 'Homeostatic process', 'Circadian process'})
+legend(Legend)
 set(legend, 'location', 'northwest')
